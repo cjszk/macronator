63a5b348effed10797352b88c4f67305626a6133
Christopher <cjszk@hotmail.com>
2018-07-15 18:39:56 -0700

    now server-side

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000' || 'https://macronator.netlify.com',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
         process.env.DATABASE_URL || 'mongodb://chris:macronator@ds127958.mlab.com:27958/macronator',
   JWT_SECRET: process.env.JWT_SECRET,
